fabbcfcee6440e2d731a09c09003a721b3642c56
Kwzii <browntravis760@gmail.com>
2024-04-07 01:23:41 -0400

    Removed useless hopper code for printers

--- src/main/java/net/kwzii/hardcashmod/block/entity/BasicMoneyPrinterBlockEntity.java
+++ src/main/java/net/kwzii/hardcashmod/block/entity/BasicMoneyPrinterBlockEntity.java
@@ -188,22 +188,4 @@ public class BasicMoneyPrinterBlockEntity extends BlockEntity implements MenuPro
             this.itemHandler.setStackInSlot(INK_SLOT, new ItemStack(emptyJar.getItem(), emptyJar.getCount()));
         }
     }
-
-    public void insertItem(ItemStack stack, BlockPos printer, BlockPos hopper) {
-        int slot;
-        System.out.println("INSERTING SINSERTNY INSERITNFG S");
-        if (printer.getY() == hopper.getY()) {
-            slot = 2;
-        } else {
-            slot = 1;
-        }
-        System.out.println("STACK SLOT IS " + slot);
-        ItemStack currentStack = itemHandler.getStackInSlot(slot);
-//        if (currentStack.isEmpty()) {
-            itemHandler.insertItem(slot, stack, false);
-//        } else if (ItemStack.isSameItemSameTags(currentStack, stack)) {
-//            currentStack.grow(stack.getCount());
-//        }
-        setChanged();
-    }
 }
